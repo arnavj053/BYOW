778e4b2c4cc9a9ddfe4b8a4c46133af31bb5d95f
Chiruhaas <chiru@berkeley.edu>
2023-11-14 15:29:45 -0800

    basics

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,40 @@
 package core;
+import tileengine.Tileset;
+import tileengine.TETile;
+import tileengine.TERenderer;
+import java.util.Random;
 
 public class World {
+    public static Random random = new Random();
+    TERenderer ter = new TERenderer();
+    public static final int WIDTH = 80;
+    public static final int HEIGHT = 40;
+    public static final int WINDOW_WIDTH = 80;
+    public static final int WINDOW_HEIGHT = 45;
+    public TETile[][] tiles;
 
-    // build your own world!
+    public World() {
+        tiles = new TETile[WIDTH][HEIGHT];
+        initializeTiles(WIDTH, HEIGHT);
+    }
 
+    private void initializeTiles(int width, int height) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+    
+    public TETile[][] getTiles() {
+        return tiles;
+    }
+
+    public static void main(String[] args) {
+        World newWorld = new World();
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        ter.renderFrame(newWorld.getTiles());
+
+    }
 }
