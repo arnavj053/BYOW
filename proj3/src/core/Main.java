62e3356319dbd1318c1530dc0a8d411a44284a79
Arnav Jain <arnavj@berkeley.edu>
2023-11-22 13:45:11 -0800

    N on new game functionality works on main menu

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -1,12 +1,29 @@
 package core;
 
 
+import tileengine.TERenderer;
+
 import java.awt.*;
 
 public class Main {
+    public static final int WIDTH = 80;
+    public static final int HEIGHT = 40;
     public static void main(String[] args) {
         MainMenu menu = new MainMenu();
-        menu.showMenu();
+        int userSelection = menu.showMenu();
+        if (userSelection == 1) {
+            long seed = menu.enterSeed();
+            World newWorld = new World (seed);
+            TERenderer ter = new TERenderer();
+            ter.initialize(WIDTH, HEIGHT);
+            ter.renderFrame(newWorld.getTiles());
+        }
+        if (userSelection == 2) {
+            //use save and load feature to load that specific game
+        }
+        if (userSelection == 3) {
+            
+        }
     }
 }
 
--- proj3/src/core/MainMenu.java
+++ proj3/src/core/MainMenu.java
@@ -10,32 +10,69 @@ import static edu.princeton.cs.algs4.StdDraw.*;
 
 
 public class MainMenu {
-    public static final int WIDTH = 500;
-    public static final int HEIGHT = 500;
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 600;
 
     public MainMenu() {
         initialDraw();
     }
 
-    private void initialDraw() {
+    public void initialDraw() {
         StdDraw.setCanvasSize(WIDTH, HEIGHT);
         StdDraw.setXscale(0, WIDTH);
         StdDraw.setYscale(0, HEIGHT);
         StdDraw.enableDoubleBuffering(); // prevents flickering and makes animations smoother
     }
 
-    public void showMenu() {
-        StdDraw.clear(StdDraw.BLACK);
+    public int showMenu() {
+        while (true) {
+            StdDraw.clear(StdDraw.BLACK);
 
+            StdDraw.setPenColor(Color.WHITE);
+            StdDraw.setFont(new Font("Times new Roman", Font.BOLD, 40));
+            StdDraw.text(WIDTH / 2.0, HEIGHT / 1.5, "CS61B: Game");
+
+            StdDraw.setFont(new Font("Times new Roman", Font.PLAIN, 25));
+            StdDraw.text(WIDTH / 2.0, HEIGHT / 2.5, "New Game (N)");
+            StdDraw.text(WIDTH / 2.0, HEIGHT / 3.5, "Load Game (L)");
+            StdDraw.text(WIDTH / 2.0, HEIGHT / 5.5, "Quit (Q)");
+
+            StdDraw.show();
+
+            if (hasNextKeyTyped()) {
+                char gameStatus = nextKeyTyped();
+                if (gameStatus == 'N' || gameStatus == 'n') {
+                    return 1;
+                } else if (gameStatus == 'L' || gameStatus == 'l') {
+                    return 2;
+                } else if (gameStatus == 'Q' || gameStatus == 'q') {
+                    return 3;
+                }
+            }
+        }
+    }
+    public long enterSeed() {
+        StdDraw.clear(Color.BLACK);
         StdDraw.setPenColor(Color.WHITE);
-        StdDraw.setFont(new Font("Arial", Font.BOLD, 40));
-        StdDraw.text(WIDTH / 2.0, HEIGHT / 1.5, "CS61B: Game");
-
-        StdDraw.setFont(new Font("Arial", Font.PLAIN, 25));
-        StdDraw.text(WIDTH / 2.0, HEIGHT / 2.5, "New Game (N)");
-        StdDraw.text(WIDTH / 2.0, HEIGHT / 3.5, "Load Game (L)");
-        StdDraw.text(WIDTH / 2.0, HEIGHT / 5.5, "Quit (Q)");
-
+        StdDraw.setFont(new Font("Times new Roman", Font.PLAIN, 20));
+        StdDraw.text(WIDTH / 2.0, HEIGHT / 7, "Enter Seed (Press S to finish):");
         StdDraw.show();
+        String input = "";
+        while (true) {
+            if (StdDraw.hasNextKeyTyped()) {
+                char c = StdDraw.nextKeyTyped();
+                if (c == 'S' || c == 's') {
+                    break; // Finish seed input
+                } else if (Character.isDigit(c)) {
+                    input += c; // Append digit to seed string
+                    // Redraw the prompt with the current seed input
+                    StdDraw.clear(Color.BLACK);
+                    StdDraw.setPenColor(Color.WHITE);
+                    StdDraw.text(WIDTH / 2.0, HEIGHT / 7.0, "Enter Seed (Press S to finish): " + input);
+                    StdDraw.show();
+                }
+            }
+        }
+        return Long.parseLong(input); // Convert string input to long
     }
 }
